33441e54ab222ac95c28e19167786a642be8932b
TranHuy2k2 <giahuytran2002@gmail.com>
2023-06-09 17:48:28 +0700

    updated communication between inventory and order service

--- inventory-service-module/src/main/java/tma/tghuy/inventoryservice/controllers/InventoryController.java
+++ inventory-service-module/src/main/java/tma/tghuy/inventoryservice/controllers/InventoryController.java
@@ -3,13 +3,20 @@ package tma.tghuy.inventoryservice.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tma.tghuy.inventoryservice.dtos.InventoryResponse;
 import tma.tghuy.inventoryservice.services.InventoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
-//    @GetMapping("/{sku-code}")
+    @GetMapping
+    public List<InventoryResponse> getInventories(@RequestParam List<String>skuCode){
+        return inventoryService.getSkuCodeStatus(skuCode);
+    }
 }
