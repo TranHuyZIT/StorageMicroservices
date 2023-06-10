f2fff82bf100f7a0511a84fc9c04249863cf6672
TranHuy2k2 <giahuytran2002@gmail.com>
2023-06-11 02:41:38 +0700

    implemented api gateway

---
+++ api-gateway/src/main/java/tma/tghuy/APIGateway.java
@@ -0,0 +1,11 @@
+package tma.tghuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGateway.class, args);
+    }
+}
