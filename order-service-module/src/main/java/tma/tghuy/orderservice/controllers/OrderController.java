b0b8b53ef2136709ccb65489f240664614d56ce1
TranHuy2k2 <giahuytran2002@gmail.com>
2023-06-15 01:09:26 +0700

    added resilence4j

--- order-service-module/src/main/java/tma/tghuy/orderservice/controllers/OrderController.java
+++ order-service-module/src/main/java/tma/tghuy/orderservice/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package tma.tghuy.orderservice.controllers;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.spring6.fallback.FallbackMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,16 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     public Map<String, String> placeOrder(@RequestBody OrderRequestDTO orderRequestDTO){
         orderService.placeOrder(orderRequestDTO);
         Map<String, String> message = new HashMap<>();
         message.put("message", "Placed order successfully");
         return message;
+    }
+    public Map<String, String> fallBackMethod(OrderRequestDTO orderRequestDTO, RuntimeException runtimeException){
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("message", "Oops, something went wrong, please order after some time");
+        return errorMap;
     }
 }
