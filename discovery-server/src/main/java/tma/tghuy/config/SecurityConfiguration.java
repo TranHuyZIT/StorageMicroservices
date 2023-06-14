478ce31b4168204d810728b1870f48a120a0a2b0
Huy Tran Gia <tghuy@tma.com.vn>
2023-06-14 23:04:45 +0700

    updated

---
+++ discovery-server/src/main/java/tma/tghuy/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+//package tma.tghuy.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration  {
+//}
